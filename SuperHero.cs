0fd4cef18ef4fc70214ada40523eae8678d50704
Idrisade <104003550+Idrisade@users.noreply.github.com>
2022-04-20 16:33:59 +0100

    Add files via upload

---
+++ SuperHero.cs
@@ -0,0 +1,18 @@
+﻿namespace SuperHeroAPI
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public DateTime? DateCreated { get; set; }
+    }
+    public class SuperHeroDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ SuperHeroController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private static List<SuperHero> Heroes = new()
+        {
+            new SuperHero
+            {
+                Id = 1,
+                Name = "Sango",
+                FirstName = "Fire",
+                LastName = "Lord",
+                City = "Osun",
+                DateCreated = DateTime.UtcNow
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<SuperHero> getAllHeroes()
+        {
+            return Ok(Heroes);
+        }
+        [HttpGet("name")]
+        public ActionResult<SuperHero> getSingleHero(string name)
+        {
+            foreach(SuperHero hero in Heroes)
+            {
+                if(hero.Name == name) return Ok(hero);  
+            }
+            return BadRequest($"Hero {name} not found");
+        }
+        [HttpPost("newHero")]
+        public ActionResult<SuperHero> createHero(SuperHero hero)
+        {
+            //hero.DateCreated = DateTime.UtcNow;
+            Heroes.Add(hero);
+            return Ok(Heroes);
+        }
+        [HttpPut("UpdateHero")]
+        public ActionResult<SuperHero> updateHero(string name, SuperHeroDto heroDto)
+        {
+            foreach(SuperHero hero in Heroes)
+            {
+                if (hero.Name == name)
+                {
+                    hero.FirstName = heroDto.FirstName;
+                    hero.LastName = heroDto.LastName;
+                    hero.City = heroDto.City;
+                    hero.DateCreated = DateTime.UtcNow;
+                    return Ok($"Hero {hero.Name} has been updated successfully");
+                }
+            }
+            return BadRequest($"Hero does not exist");
+        }
+        [HttpDelete("DeleteHero")]
+        public ActionResult<SuperHero> deleteHero(string name)
+        {
+            foreach(SuperHero hero in Heroes)
+            {
+                if(hero.Name.Equals(name) )
+                {
+                    Heroes.Remove(hero);
+                    return Ok($"Hero {name} has been removed successfully");
+                }
+            }
+            return BadRequest($"Hero {name} does not exist");
+        }
+
+    }
+}
